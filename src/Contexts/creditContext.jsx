b3759a8bb233eb46e5e59c1f1ee2b03b3334a647
muhammad-abdullah11 <abdullahworld111@gmail.com>
2026-04-13 16:36:32 +0500

    add fetch all credit from local stroage

--- src/Contexts/creditContext.jsx
+++ src/Contexts/creditContext.jsx
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import {useState, createContext, useEffect } from "react";
 
 const CreditContext = createContext();
 
@@ -7,8 +7,20 @@ export default CreditContext;
 import React from 'react'
 
 const CreditProvider = ({ children }) => {
+
+    const [credits, setCredits] = useState([]);
+
+    const fetchAllCredits = () => {
+        localStorage.getItem("credits") && setCredits(JSON.parse(localStorage.getItem("credits")));
+        setCredits(JSON.parse(localStorage.getItem("credits")) || []);
+    }
+
+    useEffect(() => {
+        fetchAllCredits();
+    }, [credits]);    
+
     return (
-        <CreditContext.Provider value={{}}>
+        <CreditContext.Provider value={{ credits, setCredits }}>
             {children}
         </CreditContext.Provider>
     )
